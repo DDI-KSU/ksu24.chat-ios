d5eacca6cf6276fb0fb016e99c8b2d121c17bd14
Milush47 <kulpievmilush@gmail.com>
2024-11-17 18:48:03 +0100

    ADDED: - Chat model(LastMessage)

---
+++ ksu24.chat-ios/ksu24.chat-ios/Models/Chat.swift
@@ -0,0 +1,28 @@
+//
+//  Chat.swift
+//  ksu24.chat-ios
+//
+//  Created by Milush Kulpiiev on 17/11/2024.
+//
+
+import Foundation
+
+struct Chat: Codable, Identifiable, Hashable {
+    var id:                         UUID
+    var name:                       String
+    var description:                String?
+    var isChannel:                  Bool
+    var isPrivate:                  Bool
+    var image:                      String?
+    var lastMessage:                LastMessage?
+    var messagesSinceLastChecked:   Int?
+    var conversationType:           Int
+}
+
+struct LastMessage: Codable, Identifiable, Hashable {
+    var id:             UUID
+    var sender:         Sender?
+    var content:        String
+    var created:        String
+    var attachments:    [String] = []
+}
