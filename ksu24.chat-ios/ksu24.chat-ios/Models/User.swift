6e3841c5424c2838d25ee960676718e84cfcbeeb
Milush47 <kulpievmilush@gmail.com>
2024-11-18 23:16:15 +0100

    CREATED: - User model

---
+++ ksu24.chat-ios/ksu24.chat-ios/Models/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  ksu24.chat-ios
+//
+//  Created by Milush Kulpiiev on 18/11/2024.
+//
+
+import Foundation
+
+struct User: Codable {
+    var access:     String
+    var refresh:    String
+}
