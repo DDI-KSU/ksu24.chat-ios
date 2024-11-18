67e6b3698108f18ac0a589da789297e01fe5bd55
Milush47 <kulpievmilush@gmail.com>
2024-11-19 00:00:06 +0100

    CREATED: - creditinals model

---
+++ ksu24.chat-ios/ksu24.chat-ios/Models/Creditinals.swift
@@ -0,0 +1,13 @@
+//
+//  Creditinals.swift
+//  ksu24.chat-ios
+//
+//  Created by Milush Kulpiiev on 18/11/2024.
+//
+
+import Foundation
+
+struct Creditinals{
+    var username: String
+    var password: String
+}
