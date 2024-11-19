6704b89d25e25216a44fa2a6c3950b3a727d0302
Milush47 <kulpievmilush@gmail.com>
2024-11-19 18:51:53 +0100

    ADDED: - messages published var - loadMessages method

--- ksu24.chat-ios/ksu24.chat-ios/ViewModels/ChatManager.swift
+++ ksu24.chat-ios/ksu24.chat-ios/ViewModels/ChatManager.swift
@@ -9,7 +9,8 @@ import Foundation
 import Combine
 
 class ChatManager: ObservableObject {
-    @Published public var chats: [Chat] = []
+    @Published public var chats:    [Chat]    = []
+    @Published public var messages: [Message] = []
     
     private var NRL:            NetworkResponseLoader
     private var cancellables = Set<AnyCancellable>()
@@ -33,5 +34,19 @@ class ChatManager: ObservableObject {
                 .store(in: &cancellables)
     }
     
-    
+    public func loadMessages(withID: UUID) {
+        NRL.loadCollactable(modelType: Message.self, endpoint: .messages(withID: withID), method: "GET")
+            .receive(on: DispatchQueue.main)
+            .sink(
+                receiveCompletion: { completion in
+                    if case .failure(let error) = completion {
+                        print("Login failed: \(error)")
+                    }
+                },
+                receiveValue: { result in
+                    self.messages = result
+                })
+                .store(in: &cancellables)
+            
+    }
 }
