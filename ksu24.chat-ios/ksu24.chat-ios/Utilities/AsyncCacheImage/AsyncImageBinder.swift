c7fff4759feb8d8af513ee1827030cc4ef9deb6e
Milush47 <kulpievmilush@gmail.com>
2024-11-20 12:12:26 +0100

    ADDED: - Caching image after loading it

--- ksu24.chat-ios/ksu24.chat-ios/Utilities/AsyncCacheImage/AsyncImageBinder.swift
+++ ksu24.chat-ios/ksu24.chat-ios/Utilities/AsyncCacheImage/AsyncImageBinder.swift
@@ -10,14 +10,21 @@ import Combine
 
 class AsyncImageBinder: ObservableObject {
     private var cancellable: AnyCancellable?
+    private var cache = AsyncImageCache.shared
     
     @Published private(set) var image: UIImage?
     
     func load(url: URL) {
+        if let image: UIImage = cache[url.absoluteString] {
+            self.image = image
+            return
+        }
+
         cancellable = URLSession.shared
             .dataTaskPublisher(for: url)
             .map {UIImage(data: $0.data)}
             .replaceError(with: nil)
+            .handleEvents(receiveOutput: { self.cache[url.absoluteString] = $0 })
             .receive(on: DispatchQueue.main)
             .assign(to: \.image, on: self)
     }
