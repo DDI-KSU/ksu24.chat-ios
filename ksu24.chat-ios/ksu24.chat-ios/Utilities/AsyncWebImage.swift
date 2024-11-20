fe93a5a12d16d89970810905cb07471d873fc038
Milush47 <kulpievmilush@gmail.com>
2024-11-20 11:43:51 +0100

    CHANGED: - EnvironmentObject -> ObservedObject
    CREATED:
    - AsyncWebImage

---
+++ ksu24.chat-ios/ksu24.chat-ios/Utilities/AsyncWebImage.swift
@@ -0,0 +1,18 @@
+//
+//  AsyncWebImage.swift
+//  ksu24.chat-ios
+//
+//  Created by Milush Kulpiiev on 20/11/2024.
+//
+
+import SwiftUI
+
+struct AsyncWebImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AsyncWebImage()
+}
