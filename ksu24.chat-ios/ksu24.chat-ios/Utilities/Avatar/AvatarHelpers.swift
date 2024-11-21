5b160903542161ad29ddf8e5d18ead686cb00e18
Milush47 <kulpievmilush@gmail.com>
2024-11-22 00:36:30 +0100

    CREATED: - AvatarHelpers(ColorExtension, AvatarPlaceHolderColor)

--- ksu24.chat-ios/ksu24.chat-ios/Utilities/Avatar/AvatarHelpers.swift
+++ ksu24.chat-ios/ksu24.chat-ios/Utilities/Avatar/AvatarHelpers.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+import SwiftUI
+
+enum AvatarPlaceHolderColor: CaseIterable {
+    case PURPLE
+    case GREEN
+    case BLUE
+    case ORANGE
+    case PINK
+    
+    var color: Color {
+        switch self {
+        case .PURPLE:
+            return Color.purple
+        case .GREEN:
+            return Color.green
+        case .BLUE:
+            return Color.blue
+        case .ORANGE:
+            return Color.orange
+        case .PINK:
+            return Color.pink
+        }
+    }
+}
+
+extension Color {
+    static func random() -> Color {
+        AvatarPlaceHolderColor.allCases.randomElement()!.color
+    }
+}
+
+extension String {
+    func takeLettersForAvatar() -> String {
+        
+        let words = self.components(separatedBy: CharacterSet.whitespacesAndNewlines).filter { !$0.isEmpty }
+        
+        let initials = words.compactMap { $0.first }.map { String($0) }
+        
+        let firstTwoInitials = initials.prefix(2).joined()
+        
+        return firstTwoInitials.uppercased()
+    }
+}
+
